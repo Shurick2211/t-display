7f08802c2826f141ac841399940db5a947989006
Shurick <shurick2211@gmail.com>
2025-02-23 20:49:50 +0200

    wait

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import st7789
 from command_handler import running_command, picture, measured
 from tdisplay_esp32.tft_config import config
 from wifi_connection import connect_wifi
+import fonts.vector.scripts as font_g
 
 def loop(sc:st7789.ST7789):
     while True:
@@ -17,8 +18,9 @@ def start():
 
     screen = config()
     screen.init()
-    picture(screen)
-
+    screen.rotation(1)
+    screen.jpg("jpg/bigbuckbunny-240x135.jpg", 0, 0, st7789.SLOW)
+    screen.draw(font_g, "Wait!", 60, 80, st7789.WHITE, 2.0)
     wlan = connect_wifi()
 
     screen.rotation(1)
